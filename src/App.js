4868699611bc54f9ddfdb348acb155c61af4772a
Michael Lai <michaeljin.lai@gmail.com>
2020-03-08 10:38:15 +1100

    Create react app project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TimerPage } from './pages/TimerPage';
+
+function App() {
+  return <TimerPage />;
+}
+
+export default App;
---
+++ src/pages/TimerPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const TimerPage = props => {
+  return (
+    <Container>
+      <div>Page goes here</div>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+
+`;
+
